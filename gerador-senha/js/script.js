e771a66fae0cd9dbb685d38cff02e0f3be74e837
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-09-19 22:05:18 -0300

    GeradorSenha

--- gerador-senha/js/script.js
+++ gerador-senha/js/script.js
@@ -1,22 +1,30 @@
 const senhaBox = document.getElementById("senha");
-const tamanho = 8;
+const tamanho = 12;
 
 const maiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const minusculo = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
+const numero = "0123456789";
 const simbolos = "@#$%^&*()_+~|{[]></-=";
+
+const todosCaracteres = maiusculo + minusculo + numero + simbolos;
 
 
 function criarSenha(){
     let senha = "";
     senha += maiusculo[Math.floor(Math.random() * maiusculo.length)];
     senha += minusculo[Math.floor(Math.random() * minusculo.length)];
-    senha += number[Math.floor(Math.random() * number.length)];
+    senha += numero[Math.floor(Math.random() * numero.length)];
     senha += simbolos[Math.floor(Math.random() * simbolos.length)];
 
         while(tamanho > senha.length ){
-            
+            senha += todosCaracteres[Math.floor(Math.random() * todosCaracteres.length)];
         }
+        senhaBox.value = senha;
 
+}
+
+function copiarSenha(){
+    senhaBox.select();
+    navigator.clipboard.writeText(senhaBox.value);
 }
 

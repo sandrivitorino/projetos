85e19a142332c879e9a9a729e9f678764722b3e1
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-09-20 00:20:20 -0300

    NotaApp

--- nota-app/js/script.js
+++ nota-app/js/script.js
@@ -2,6 +2,16 @@ const containerNotas = document.querySelector(".notas-container");
 const criarBtn = document.querySelector(".btn");
 let notas = document.querySelectorAll(".input-box");
 
+function mostrarNotas(){
+    containerNotas.innerHTML = localStorage.getItem("notas");
+}
+
+mostrarNotas();
+
+function updateStorage(){
+    localStorage.setItem("notas", containerNotas.innerHTML);
+}
+
 criarBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -9,6 +19,28 @@ criarBtn.addEventListener("click", ()=>{
     inputBox.setAttribute("contenteditable", "true");
     img.src = "images/delete.png";
     containerNotas.appendChild(inputBox).appendChild(img);
-    
 
+
+})
+
+containerNotas.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    } 
+    else if(e.target.tagName === "P"){
+        notas = document.querySelectorAll(".input-box");
+        notas.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
 })

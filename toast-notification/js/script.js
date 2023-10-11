22034e2c86766b81badfb42d24901e3afaad0715
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-10-11 16:14:51 -0300

    tn

--- toast-notification/js/script.js
+++ toast-notification/js/script.js
@@ -1,9 +1,12 @@
 let toastbox = document.getElementById('toastbox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
 
-function showToast(){
+function showToast(msg){
    let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = 'success';
+    toast.innerHTML = msg;
     toastbox.appendChild(toast);
-    
+
 }

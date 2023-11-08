a278b0ebc6b95689cac22652b97ad29c1e3ca9d6
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-11-08 03:06:50 -0300

    done

--- show-on-off-password/js/script.js
+++ show-on-off-password/js/script.js
@@ -1,6 +1,6 @@
 var sen = document.getElementById("password");
-var msg = document.getElementById("messege");
-var tam = document.getElementById("tamanho");
+var msg = document.getElementById("message");
+var str = document.getElementById("strenght");
 
 sen.addEventListener('input', () => {
     if(sen.value.length > 0){
@@ -8,5 +8,20 @@ sen.addEventListener('input', () => {
     }
     else{
         msg.style.display = "none";
+    }
+    if(sen.value.length < 4){
+        str.innerHTML = "fraca";
+        sen.style.borderColor = "#ff5925";
+        msg.style.color = "#ff5925";
+    }
+    else if(sen.value.length >= 4 && sen.value.length < 8){
+        str.innerHTML = "media";
+        sen.style.borderColor = "yellow";
+        msg.style.color = "yellow";
+    }
+    else if (sen.value.length > 8){
+        str.innerHTML = "forte";
+        sen.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
     }
 })
